02fef50e4eab803ff16f93f8c764018b99082ed9
Natdanai <natdanai.thaiyala@gmail.com>
2024-02-06 14:47:34 +0700

    fix config between tailwind and MUI

--- src/app/mui/page.tsx
+++ src/app/mui/page.tsx
@@ -8,7 +8,9 @@ export default function MUIDemo({}: Props) {
   return (
     <div>MUIDemo
         <Stack spacing={2} direction="row">
-            
+            <Button variant="text">Text</Button>
+            <Button variant="contained">Contained</Button>
+            <Button variant="outline">Outlined</Button>
         </Stack>
     </div>
   )
