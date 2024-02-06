43f32a8cd92f7426eb12e023d76bd678bf7ae8d4
Natdanai <natdanai.thaiyala@gmail.com>
2024-02-06 14:40:01 +0700

    setup workshop is using material ui MUI

---
+++ src/app/mui/page.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+
+type Props = {}
+
+export default function MUIDemo({}: Props) {
+  return (
+    <div>MUIDemo
+        <Stack spacing={2} direction="row">
+            
+        </Stack>
+    </div>
+  )
+}
