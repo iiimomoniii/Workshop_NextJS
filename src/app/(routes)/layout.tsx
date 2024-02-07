5e802f622c802365a413fc624207b3500a111f9a
Natdanai <natdanai.thaiyala@gmail.com>
2024-02-07 17:00:20 +0700

    Remove

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -6,28 +6,6 @@ import Sidebar from '../_components/layout/Sidebar';
 import DrawerHeader from '../_components/layout/DrawerHeader';
 import { styled } from '@mui/material';
 
-const drawerWidth = 240;
-
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
-  open?: boolean;
-}>(({ theme, open }) => ({
-  flexGrow: 1,
-  padding: theme.spacing(3),
-  transition: theme.transitions.create('margin', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  marginLeft: `-${drawerWidth}px`,
-  ...(open && {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  }),
-}));
-
-
 type Props = {
   children: React.ReactNode
 }
