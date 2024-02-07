e4e42d80fcfd7bcbab7578dd1a1e962cb8510c80
Natdanai <natdanai.thaiyala@gmail.com>
2024-02-07 14:27:54 +0700

    Update
    import image

--- src/app/(routes)/stock/page.tsx
+++ src/app/(routes)/stock/page.tsx
@@ -1,11 +1,15 @@
 import { Box } from '@mui/material'
 import React from 'react'
-
+import Image from 'next/image';
 type Props = {}
 
 export default function Stock({}: Props) {
   return (
     // <div style={{ marginTop: 8 }}>Stock</div> margin sx 1 : style 8
-    <Box sx={{ mt:1 }}> Stock</Box>
+    // <Box sx={{ mt:1 }}> Stock</Box>
+    <Box sx={{ mt:1 }}>
+      Stock
+      <Image src="/static/img/next_register.jpg" width={180} height={35} alt="logo" />
+    </Box>
   )
 }
