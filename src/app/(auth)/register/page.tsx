621a4d2eb81303f6c917b760c03e807e021ad37b
Natdanai <natdanai.thaiyala@gmail.com>
2024-02-07 18:04:57 +0700

    Update
    add alert username and password from useState

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,15 +1,27 @@
 "use client";
 import { Box, Button, Card, CardContent, CardMedia, InputAdornment, TextField, Typography } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import * as Icons from '@mui/icons-material';
+
+interface User {
+  username : string;
+  password : string;
+}
+
 type Props = {}
 
 export default function Register({ }: Props) {
 
+  const [user, setUser] = useState<User>({username:"",password:""})
+
   const showForm = () => {
-    return <form>
+    return <form onSubmit={()=> {
+      alert(JSON.stringify(user))
+    }}>
       {/* Username */}
       <TextField
+        // set username and maintain old password
+        onChange={e=> setUser({username: e.target.value, password: user.password})}
         variant="outlined"
         margin="normal"
         fullWidth
@@ -27,6 +39,8 @@ export default function Register({ }: Props) {
 
         {/* Password */}
         <TextField
+        // set password and maintain old username
+        onChange={e=> setUser({username: user.username, password: e.target.value})}
         variant="outlined"
         margin="normal"
         fullWidth
