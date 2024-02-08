777328ec93f3fa522cd66a2d3bad3913cc660bc8
Natdanai <natdanai.thaiyala@gmail.com>
2024-02-08 09:45:27 +0700

    Update
    add validate invalid form of username and password

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,6 +3,8 @@ import { Box, Button, Card, CardContent, CardMedia, InputAdornment, TextField, T
 import React, { useState } from 'react'
 import * as Icons from '@mui/icons-material';
 import { Controller, useForm } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 interface User {
   username: string;
   password: string;
@@ -13,10 +15,17 @@ type Props = {}
 export default function Register({ }: Props) {
 
   const initialValue: User = { username: "", password: "" }; //default value = ""
+  //validate usename and password by yup lib
+  const formValidateSchema = Yup.object().shape({
+    username : Yup.string().required("Email is Required").trim(),
+    password : Yup.string().required("Password is Required").trim()
+  })
 
-  const { control, handleSubmit } = useForm<User>({
+  //send error message by errors in formState
+  const { control, handleSubmit , formState: {errors} } = useForm<User>({
     defaultValues: initialValue,
-    resolver: undefined
+    //bind formValidateSchema to resolver
+    resolver: yupResolver(formValidateSchema)
   });
 
   const showForm = () => {
@@ -31,8 +40,10 @@ export default function Register({ }: Props) {
           <TextField
             // onChange={field.onChange} value={field.value} = {...field}
             {...field}
-            error={true}
-            helperText={"Invalid Username"}
+            // add condition check when errors of usename has message will show error message 
+            error={Boolean(errors.username?.message)}
+            // errors message from formValidateSchema 
+            helperText={errors.username?.message?.toString()}
             variant="outlined"
             margin="normal"
             fullWidth
@@ -57,6 +68,8 @@ export default function Register({ }: Props) {
         render={({ field }) => (
           <TextField
             {...field}
+            error={Boolean(errors.password?.message)}
+            helperText={errors.password?.message?.toString()}
             variant="outlined"
             margin="normal"
             fullWidth
