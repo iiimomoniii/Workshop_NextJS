f1dfbf6872af0e711a42f7709052b887e24280d0
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2024-03-22 18:10:16 +0700

    Update
    create userSlice and register in store

---
+++ src/store/slices/userSlice.ts
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+    name: "user", // name of slice
+    initialState: {}, //init start value
+    reducers: {} // logic
+});
+
+export default userSlice.reducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
+import userReducer from "@/store/slices/userSlice"; 
 
-const reducer = {};
+const reducer = { userReducer }; //register userSlice
 
+//use store for share all values in project Example share values between header and body
 export const store = configureStore({
     reducer,
     devTools: process.env.NODE_ENV === 'development', //Protect Log when env Production
