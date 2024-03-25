765c3a29c59e9585ec6a20a6071e45298670e201
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2024-03-25 22:15:36 +0700

    Update
    add service for call register api from backend

--- src/store/slices/userSlice.ts
+++ src/store/slices/userSlice.ts
@@ -1,16 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../store";
+import * as service from "@/services/Service";
+interface SignAction {
+    username: string;
+    password: string;
+}
 
+const signUp = createAsyncThunk("user/signUp", async (user: SignAction) => {
+    //contain logic send request to backend for registration
+    const response = await service.signUp(user);
+    return response;
+}
+
+
+);
 const userSlice = createSlice({
     name: "user", // name of slice
-    initialState: { count:10}, //init start value
+    initialState: { count: 10 }, //init start value
     reducers: {
-        add:(state) => {
+        add: (state) => {
             state.count++; // assign value when click will update count
         }
-    } // logic
+    }, // logic
+    extraReducers: (builder => {
+        //exception signUp service
+        builder.addCase(signUp.fulfilled, (state,action)=> {
+            state.count++;
+        });
+    }),
 });
 
 export default userSlice.reducer;
-export const userSelector = (state:RootState) => state.userReducer;
+export const userSelector = (state: RootState) => state.userReducer;
 export const { add } = userSlice.actions;
